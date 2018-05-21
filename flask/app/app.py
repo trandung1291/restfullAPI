78e737d641c3dc1ff0294dfa62a117f72fa682ce
tran van dung <dungtranvan@admicro.vn>
2018-05-21 11:19:07 +0700

    test

--- flask/app/app.py
+++ flask/app/app.py
@@ -13,9 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'dung'
 api = Api(app)
 
-@app.before_first_request
-def creat_tables():
-	db.create_all()
+
 jwt = JWT(app,authenticate,identity) #/auth
 
 api.add_resource(Item,'/item/<string:name>')
